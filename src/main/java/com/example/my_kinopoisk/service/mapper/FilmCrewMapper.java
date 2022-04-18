495cd5718b2e95b420295e065eea478c25204d84
venommile <galtsev.mishail@gmail.com>
2022-04-18 18:52:53 +0300

    add person_id to FilmcrewDto Fix filmcrew data in filmcrew.csv

--- src/main/java/com/example/my_kinopoisk/service/mapper/FilmCrewMapper.java
+++ src/main/java/com/example/my_kinopoisk/service/mapper/FilmCrewMapper.java
@@ -5,7 +5,6 @@ import com.example.my_kinopoisk.domain.dto.FilmCrewInPerson;
 import com.example.my_kinopoisk.domain.dto.FilmCrewMovieViewDto;
 import com.example.my_kinopoisk.domain.entity.FilmCrew;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface FilmCrewMapper {
